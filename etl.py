3c1757255eda60cf36a70eb60fc56a07ef3c1c9d
Salem Chniguir <salemchniguir2@gmail.com>
2019-11-19 13:03:03 +0100

    the etl process

--- etl.py
+++ etl.py
@@ -215,36 +215,64 @@ class TransformCDR(Transform):
     Methods
     ----------
     setTableCDR(df,month):
-        transform the dataFrame df to a cdrTable of the choosen month
+        transform the dataFrame df to a cdr(call detail record)
+        dataFrame of the choosen month
     """
 
 
     def setTableCDR(self,df,month):
+        """
+        transform the dataFrame df to a dataFrame cdr(call datail record)
+        of the choosen month and return df_cdr
 
+        Parameters
+        ------------
+        df: dataFrame
+            the dataFrame to be transformed
+        dictio: str
+            the choosen month of the cdr dataFrame
+        """
         cdr= self.sqlContext.createDataFrame(self.sparkContext.emptyRDD(),df.schema)
         cdr=cdr.filter(cdr.DURATION>10)
-
         df1=df.select("FROM_SUBSCRIBER_ID","TO_SUBSCRIBER_ID").where(df.CALL_DATE==month)
         df2=df.select("TO_SUBSCRIBER_ID","FROM_SUBSCRIBER_ID").where(df.CALL_DATE==month)
-
-
         df3=df1.intersect(df2)
         df3.registerTempTable("cdrSelected")
         df.where(df.CALL_DATE==month).registerTempTable("cdr")
-
         df_cdr=self.sqlContext.sql("select CALL_DATE,cdr.FROM_SUBSCRIBER_ID,cdr.TO_SUBSCRIBER_ID,cdr.A_NUMBER_NETWORK,cdr.B_NUMBER_NETWORK,CALLS,SMS,DURATION,CALLING_DAYS \
         from cdr,cdrSelected where \
         (cdr.FROM_SUBSCRIBER_ID=cdrSelected.FROM_SUBSCRIBER_ID and cdr.TO_SUBSCRIBER_ID=cdrSelected.TO_SUBSCRIBER_ID)")
-
         return df_cdr
 
+class TransformComportment(Transform):
+    """
+    A Child Class of Transform Class used to transform the dataFrame to a Comportment of the users
 
+    Methods
+    ----------
+    setTableComportment(df1,df2,df3,df4,df5):
+        transform the dataFrames df1,df2,df3,df4 and df5 to a dataFrame that
+        contains  the Comportment of the users
+    """
+    def setTableComportment(self,df1,df2,df3,df4,df5):
+        """
+        transform the dataFrames df1,df2,df3,df4 and df5 to a dataFrame that
+        contains  the Comportment of the users and returns result
 
+        Parameters
+        ------------
 
-# classe permettant d'avoir une table qui carecterise le comportement de chaque client
-class TransformComportement(Transform):
-    def setTableComportement(self,df1,df2,df3,df4,df5):
-
+        df1: dataFrame
+            the dataFrame to be transformed
+        df2: dataFrame
+            the dataFrame to be transformed
+        df3: dataFrame
+            the dataFrame to be transformed
+        df4: dataFrame
+            the dataFrame to be transformed
+        df5: dataFrame
+            the dataFrame to be transformed
+        """
         d1=df1.select('CODE_CONTRAT','MONTH_DT','NB_APPEL','DUREE_APPEL','NB_APPEL_TT_GSM','DUREE_APPEL_TT_GSM','DUREE_APPEL_TT_FIXE','NB_APPEL_TT_FIXE',)
         d1 =d1.selectExpr("CODE_CONTRAT as CODE_CONTRAT", "NB_APPEL as NB_APPEL_out","DUREE_APPEL as DUREE_APPEL_out","NB_APPEL_TT_GSM as NB_APPEL_TT_GSM_out","DUREE_APPEL_TT_GSM as DUREE_APPEL_TT_GSM_out","DUREE_APPEL_TT_FIXE as DUREE_APPEL_TT_FIXE_out","NB_APPEL_TT_FIXE as NB_APPEL_TT_FIXE_out")
         d2=df2.select('CODE_CONTRAT','NB_APPEL','DUREE_APPEL','NB_APPEL_TT_GSM','DUREE_APPEL_TT_GSM','DUREE_APPEL_TT_FIXE','NB_APPEL_TT_FIXE',)
@@ -255,11 +283,34 @@ class TransformComportement(Transform):
 
         result = d1.join(d2, on="CODE_CONTRAT").join(d3, on="CODE_CONTRAT").join(d4, on="CODE_CONTRAT").join(d5, on="CODE_CONTRAT")
         return result
-# classe permettant d'enregistrer les données dans l'SGBD Oracle
-class Load():
 
-    def loadDataFrame(self,dataFrame,tableName,user,password,mode="Append"):
-        dataFrame.write.mode(mode).format("jdbc")\
+
+class Load():
+    """
+    A Class used to load the dataFrames to the oracle DBMS
+
+    Methods
+    ----------
+    loadDataFrame(df,tableName,user,password,mode="Append"):
+        load the dataFrame to a table with a choosen tableName
+    """
+    def loadDataFrame(self,df,tableName,user,password,mode="Append"):
+        """
+        load the dataFrame df to a table with a choosen tableName
+        Parameters
+        ------------
+        df: dataFrame
+            the dataFrame that will be loaded to the Oracle DB
+        tableName: str
+            the name of the table where you will load the dataFrame
+        user: str
+            the name of the user of the Oracle DBMS
+        password: str
+            the password of the user account in the Oracle DBMS
+        mode: str
+            the mode of loading (default: Append)
+        """
+        df.write.mode(mode).format("jdbc")\
          .option("url","jdbc:oracle:thin:@localhost:1521:xe")\
          .option("dbtable", tableName)\
          .option("user", user)\
@@ -268,28 +319,48 @@ class Load():
          .save()
 
 
-# classe permettant de faire l'ETL
 class ETL():
-    def __init__(self,sqlContext,sparkContext,user,password):
-        self.sqlContext=sqlContext
-        self.sparkContext=sparkContext
+    """
+    A Class used to do the ETL process (extract, transform, load)
+
+    Methods
+    ----------
+    job(month):
+        the job to be executed to do the etl process of the choosen month
+    """
+
+    def __init__(self,sqlc,sc,user,password):
+        """
+        Parameters
+        ------------
+        sqlc: SQLContext
+            the SQLContext of the Application
+        sc: sparkContext
+            the sparkContext of the Application
+        user: str
+            the name of the user
+        password: str
+            the password of the user
+        """
+        self.sqlContext=sqlc
+        self.sparkContext=sc
         self.DataFrames=[]
         self.user=user
         self.password=password
         self.Extracters=[]
         self.Extracters.append(ExtractFromOracle(self.sqlContext,"jdbc:oracle:thin:@localhost:1521:xe","dw_cla_monthly_trafic_msc",self.user,self.password))
-        #e1=ExtractFromOracle(self.sqlContext,"jdbc:oracle:thin:@localhost:1521:xe","FACT_USAGE_MONTHLY_SORTANT_B","telecom","97908631")
-        #e2=ExtractFromOracle(self.sqlContext,"jdbc:oracle:thin:@localhost:1521:xe","FACT_USAGE_MONTHLY_ENTRANT_B","telecom","97908631")
-        #e3=ExtractFromOracle(self.sqlContext,"jdbc:oracle:thin:@localhost:1521:xe","DIM_CONTRACT_D","telecom","97908631")
-        #e4=ExtractFromOracle(self.sqlContext,"jdbc:oracle:thin:@localhost:1521:xe","FACT_USAGE_MOUNTHLY_DATA_B","telecom","97908631")
-        #e5=ExtractFromOracle(self.sqlContext,"jdbc:oracle:thin:@localhost:1521:xe","DIM_PARC_RGS_D","telecom","97908631")
 
         self.TransformCDR=TransformCDR(self.sqlContext,self.sparkContext)
-        #self.TransformComportement=TransformComportement(self.sqlContext,self.sparkContext)
         self.loder=Load()
 
-    # job etl qui va ettre executé chaque mois
     def job(self,month):
+        """
+        the job to be executed to do the etl process of the choosen month
+
+        ------------
+        month: str
+            the month when the etl process will be executed
+        """
         for e in self.Extracters:
             self.DataFrames.append(e.extract())
         for i in range(len(self.DataFrames)):
@@ -297,9 +368,7 @@ class ETL():
         castColumns=[('CALLS','int'),('DURATION','int'),('SMS','int'),('CALLING_DAYS','int')]
         self.DataFrames[0]=self.TransformCDR.CastColumns(self.DataFrames[0],castColumns)
         cdrMonth=self.TransformCDR.setTableCDR(self.DataFrames[0],month)
-        #df2=self.TransformComportement.setTableComportement(self.DataFrames[1],self.DataFrames[2],self.DataFrames[3],self.DataFrames[4],self.DataFrames[5])
         self.loder.loadDataFrame(cdrMonth,"CDR_FRIENDS",self.user,self.password,"Append")
-        #self.loder.loadDataFrame(df2,"comportement",self.user,self.password)
 
 
 
@@ -329,7 +398,7 @@ def main():
     sqlContext = SQLContext(sc)
 
 
-    etl=ETL(sqlContext,sc,"telecom","97908631")
+    etl=ETL(sqlContext,sc,"user_name","your_password")
     etl.job("31MAY2019:00:00:00")
     sc.stop()
     print("end")
